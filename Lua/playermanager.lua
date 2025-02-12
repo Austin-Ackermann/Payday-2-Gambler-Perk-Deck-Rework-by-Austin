d44273461b0ef7e988d7d9194b0ce416cc63ce06
Clumsyhunt3R <austinackermann1999@gmail.com>
2025-02-12 18:16:45 +0200

    100% dodge... for some reason

--- Lua/playermanager.lua
+++ Lua/playermanager.lua
@@ -1,4 +1,4 @@
-require("lib/units/pickups/AmmoClip")
+-- require("lib/units/pickups/AmmoClip")
 
 -- Hooks.PostHook(PlayerManager, "init", "gambler_playerman_init_byp", function()
 --     self.gambler_lucky_dodge_stacks = 40
@@ -10,12 +10,19 @@ require("lib/units/pickups/AmmoClip")
 --since it no longer overwrites the dodge calc function (at least, not really)
 local orig_calc_dodge = PlayerManager.skill_dodge_chance
 function PlayerManager:skill_dodge_chance(...)
-    local {stacks, diff} = AmmoClip.getGamblerDodgeInfo()
+    -- local {stacks, diff} = AmmoClip.getGamblerDodgeInfo()
+    -- local stacks = AmmoClip.gambler_lucky_dodge_stacks
+    -- local stacks = 0.40
+    -- local diff = -0.02
     local chance = 0
+    
+    if chance < 420 then
+        -- chance = chance + stacks + diff
+        chance = chance + 1
+    end
+    chance = chance + orig_calc_dodge(self,...)
     -- local stacks = tweak_data.upgrades.values.player.gambler_lucky_dodge_stacks
     -- local diff = tweak_data.upgrades.values.player.gambler_dodge_difference
-	chance = chance + orig_calc_dodge(self,...)
-    chance = chance + stacks + diff
     
     return chance
 end
