8e6a0566bfcc2df431e8694e6f5fd02a4d60a8e5
Austin <133873488+Austin-Ackermann@users.noreply.github.com>
2025-02-14 10:13:41 +0200

    Add files via upload

---
+++ Lua/skilltree.lua
@@ -0,0 +1,311 @@
+local skill_data = SkillTreeTweakData.init
+function SkillTreeTweakData:init(tweak_data)
+	skill_data(self, tweak_data)
+	
+	-- defining common cards
+	local deck2 = {
+		upgrades = {
+			"weapon_passive_headshot_damage_multiplier"
+		},
+		cost = 300,
+		icon_xy = {1, 0},
+		name_id = "menu_deckall_2",
+		desc_id = "menu_deckall_2_desc"
+	}
+	local deck4 = {
+		upgrades = {
+			"passive_player_xp_multiplier",
+			"player_passive_suspicion_bonus",
+			"player_passive_armor_movement_penalty_multiplier"
+		},
+		cost = 600,
+		icon_xy = {3, 0},
+		name_id = "menu_deckall_4",
+		desc_id = "menu_deckall_4_desc"
+	}
+	local deck6 = {
+		upgrades = {
+			"armor_kit",
+			"player_pick_up_ammo_multiplier"
+		},
+		cost = 1600,
+		icon_xy = {5, 0},
+		name_id = "menu_deckall_6",
+		desc_id = "menu_deckall_6_desc"
+	}
+	local deck8 = {
+		upgrades = {
+			"weapon_passive_damage_multiplier",
+			"passive_doctor_bag_interaction_speed_multiplier"
+		},
+		cost = 3200,
+		icon_xy = {7, 0},
+		name_id = "menu_deckall_8",
+		desc_id = "menu_deckall_8_desc"
+	}
+
+	-- creating a new Gambler deck
+	local newGambler = {
+		{
+			cost = 200,
+			desc_id = "menu_deck10_1_desc",
+			short_id = "menu_deck10_1_short",
+			name_id = "menu_deck10_1",
+			upgrades = {
+				"temporary_loose_ammo_restore_health_1",
+				"player_gain_life_per_players"
+			},
+			icon_xy = {
+				4,
+				5
+			}
+		},
+		deck2,
+		{
+			cost = 400,
+			desc_id = "menu_deck10_3_desc",
+			short_id = "menu_deck10_3_short",
+			name_id = "menu_deck10_3",
+			upgrades = {
+				"temporary_loose_ammo_give_team",
+				"player_passive_health_multiplier_1",
+				"player_passive_health_multiplier_2"
+			},
+			icon_xy = {
+				5,
+				5
+			}
+		},
+		deck4,
+		{
+			cost = 1000,
+			desc_id = "menu_deck10_5_desc",
+			short_id = "menu_deck10_5_short",
+			name_id = "menu_deck10_5",
+			upgrades = {
+				"player_loose_ammo_restore_health_give_team",
+				"player_passive_health_multiplier_3"
+			},
+			icon_xy = {
+				6,
+				5
+			}
+		},
+		deck6,
+		{
+			cost = 2400,
+			desc_id = "menu_deck10_7_desc",
+			short_id = "menu_deck10_7_short",
+			name_id = "menu_deck10_7",
+			upgrades = {
+				"temporary_loose_ammo_restore_health_2"
+			},
+			icon_xy = {
+				7,
+				5
+			}
+		},
+		deck8,
+		{
+			cost = 4000,
+			desc_id = "menu_deck10_9_desc",
+			short_id = "menu_deck10_9_short",
+			name_id = "menu_deck10_9",
+			upgrades = {
+				"player_passive_loot_drop_multiplier",
+				"temporary_loose_ammo_restore_health_3",
+				-- "temporary_gam_luk_1" 
+			},
+			icon_xy = {
+				0,
+				6
+			}
+		},
+		name_id = "menu_st_spec_10",
+		desc_id = "menu_st_spec_10_desc",
+		category = "supportive"
+	}
+	
+	-- new dodge mechanic -- as soon as it is equiped, the game crashes: tried to index null value in playermanager. Turns out it was because this comment was next to it. 
+
+
+	-- finding the gambler deck
+	-- for deckIndex = i in self.specializations do
+	-- 	if i.name_id = "menu_st_spec_10" do
+	-- 		-- replacing the old deck with our new one
+	-- 		self.specializations[deckIndex] = newGambler
+	-- 		break
+	-- 	end
+	-- end
+
+	-- replacing the grinder deck with our new one
+	self.specializations[10] = newGambler 
+	
+	-- table.insert(self.specializations, newGambler)
+	
+	-- local newDeck = 	{
+	-- 	name_id = "menu_st_spec_0",
+	-- 	desc_id = "menu_st_spec_0_desc",
+	-- 	{
+	-- 		upgrades = {
+	-- 	 	},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_1",
+	-- 		desc_id = "menu_deck0_1_desc"
+	-- 	},			
+	-- 	{
+	-- 		upgrades = {
+	-- 			},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 5},
+	-- 		name_id = "menu_deck0_2",
+	-- 		desc_id = "menu_deck0_2_desc"
+	-- 	},
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_3",
+	-- 		desc_id = "menu_deck0_3_desc"
+	-- 	},
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_4",
+	-- 		desc_id = "menu_deck0_4_desc"
+	-- 	},
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 0},
+	-- 		name_id = "menu_deck0_5",
+	-- 		desc_id = "menu_deck0_5_desc"
+	-- 	},
+
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_7",
+	-- 		desc_id = "menu_deck0_7_desc"
+	-- 	},
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_8",
+	-- 		desc_id = "menu_deck0_8_desc"
+	-- 	},
+	-- 	{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_9",
+	-- 		desc_id = "menu_deck0_9_desc"
+	-- 	},
+	-- 			{
+	-- 		upgrades = {
+	-- 		},
+	-- 		cost = 0,
+	-- 		icon_xy = {900, 8},
+	-- 		name_id = "menu_deck0_6",
+	-- 		desc_id = "menu_deck0_6_desc"
+	-- 	}
+	-- 	}
+		-- )
+			
+-- table.insert(self.specializations,
+-- 			{
+-- 			name_id = "menu_st_spec_00",
+-- 			desc_id = "menu_st_spec_00_desc",
+-- 			{
+-- 				upgrades = {
+-- 				},
+-- 				cost = 0,
+-- 				icon_xy = {900, 7},
+-- 				name_id = "menu_deck00_1",
+-- 				desc_id = "menu_deck00_1_desc"
+-- 			},
+-- 	{	
+-- 	upgrades = {
+-- 			"weapon_passive_headshot_damage_multiplier"
+-- 		},
+-- 		cost = 300,
+-- 		icon_xy = {1, 0},
+-- 		name_id = "menu_deckall_2",
+-- 		desc_id = "menu_deckall_2_desc"
+-- 	},
+-- 			{
+-- 				upgrades = {
+-- 				},
+-- 				cost = 0,
+-- 				icon_xy = {900, 7},
+-- 				name_id = "menu_deck0_3",
+-- 				desc_id = "menu_deck0_3_desc"
+-- 			},
+-- 			{
+-- 		upgrades = {
+-- 			"passive_player_xp_multiplier",
+-- 			"player_passive_suspicion_bonus",
+-- 			"player_passive_armor_movement_penalty_multiplier"
+-- 		},
+-- 		cost = 600,
+-- 		icon_xy = {3, 0},
+-- 		name_id = "menu_deckall_4",
+-- 		desc_id = "menu_deckall_4_desc"
+-- 	},
+-- 			{
+-- 				upgrades = {
+-- 				},
+-- 				cost = 0,
+-- 				icon_xy = {900, 7},
+-- 				name_id = "menu_deck0_5",
+-- 				desc_id = "menu_deck0_5_desc"
+-- 			},
+-- 			{
+-- 		upgrades = {
+-- 			"armor_kit",
+-- 			"player_pick_up_ammo_multiplier"
+-- 		},
+-- 		cost = 1600,
+-- 		icon_xy = {5, 0},
+-- 		name_id = "menu_deckall_6",
+-- 		desc_id = "menu_deckall_6_desc"
+-- 	},
+-- 			{
+-- 				upgrades = {
+-- 				},
+-- 				cost = 0,
+-- 				icon_xy = {900, 7},
+-- 				name_id = "menu_deck0_7",
+-- 				desc_id = "menu_deck0_7_desc"
+-- 			},
+-- 			{
+-- 		upgrades = {
+-- 			"weapon_passive_damage_multiplier",
+-- 			"passive_doctor_bag_interaction_speed_multiplier"
+-- 		},
+-- 		cost = 3200,
+-- 		icon_xy = {7, 0},
+-- 		name_id = "menu_deckall_8",
+-- 		desc_id = "menu_deckall_8_desc"
+-- 	},
+-- 			{
+-- 				upgrades = {
+-- 				},
+-- 				cost = 0,
+-- 				icon_xy = {900, 7},
+-- 				name_id = "menu_deck0_9",
+-- 				desc_id = "menu_deck0_9_desc"
+-- 			}		
+-- 		}
+-- 		)
+end
---
+++ Lua/upgradestweak.lua
@@ -0,0 +1,72 @@
+Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "upgradestweak_init_byp", function(self)
+	-- Lowering gambler health and ammo gain but removing the cooldowns. 
+	
+	-- player healing
+	self.values.temporary.loose_ammo_restore_health = {}
+	for i, data in ipairs(self.loose_ammo_restore_health_values) do
+		local base = self.loose_ammo_restore_health_values.base -- AKA 8 hp in game
+		
+		table.insert(self.values.temporary.loose_ammo_restore_health, {
+			{
+				math.ceil((base + data[1]) * 0.2), -- lowering the health regen to compensate for the cooldown change
+				math.ceil((base + data[2]) * 0.2)
+			},
+			0 -- original: self.loose_ammo_restore_health_values.cd AKA 3 seconds in game
+		})
+	end
+	
+	-- teammates healing
+	for i, data in ipairs(self.loose_ammo_restore_health_values) do
+		local base = self.loose_ammo_restore_health_values.base -- AKA 8 hp in game
+		
+		table.insert(self.values.temporary.loose_ammo_restore_health, {
+			{
+				math.ceil((base + data[1]) * 0.2), -- lowering the health regen to compensate for the cooldown change
+				math.ceil((base + data[2]) * 0.2)
+			},
+			self.loose_ammo_restore_health_values.cd
+		})
+		
+	end
+	
+	-- teammates ammo
+	self.loose_ammo_give_team_ratio = 0.1 -- default 0.5 AKA 50% of player ammo gain ingame.
+	self.loose_ammo_give_team_health_ratio = 0.3 -- default: 0.5 AKA 50% of player hp gain ingame.
+	
+	-- player ammo 
+	self.values.temporary.loose_ammo_give_team = {
+		{
+			true,
+			0 -- default: 5 AKA 5 second cooldown between ammo pickups. 
+		}
+	}
+end)
+
+Hooks:PostHook(UpgradesTweakData, "_temporary_definitions", "upgradestweak_tmp_definitions_byp", function(self)
+	-- gambler's dodge
+	-- When picking up ammo, add 5% dodge chance, up to 40% increase. Chance to lose 10% dodge chance instead (cannot go below 0), which depends on the percentage of ammo you have remaining (max 50% chance).
+	self.gambler_max_lucky_dodge_stacks = 0.5 -- default: 40 AKA 40% dodge chance in game
+	self.gambler_lucky_dodge_incriment = 0.1 -- default: 0.1 AKA 10% dodge chance in game
+	self.gambler_lucky_dodge_decriment = -0.05 -- default: -0.05 AKA -5% dodge chance in game
+	self.gambler_unlucky_chance_cap = 0.75 -- default: 0.75 AKA 75% chance of failure max.
+    self.gambler_unlucky_chance_position = 1 -- default: 1 AKA unluck caps out at 100% ammo remaining.
+
+	-- self.definitions.player_gambler_dodge = {
+	-- 	name_id = "player_gambler_dodge",
+	-- 	category = "feature",
+	-- 	upgrade = {
+	-- 		value = 1,
+	-- 		upgrade = "gambler_dodge_perk",
+	-- 		category = "player"
+	-- 	}
+	-- }
+	-- self.definitions.temporary_gam_luk_1 = {
+	-- 	name_id = "menu_temporary_gam_luk",
+	-- 	category = "temporary",
+	-- 	upgrade = {
+	-- 		value = 1,
+	-- 		upgrade = "gam_luk",
+	-- 		category = "temporary"
+	-- 	}
+	-- }
+end)
